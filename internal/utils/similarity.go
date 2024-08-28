e86f69ad36d26e83d47498b81e3780b319809632
nubesFilius <eperezlarosa@outlook.com>
2024-08-28 15:26:33 -0400

    fix_cf_test_function

--- internal/utils/similarity.go
+++ internal/utils/similarity.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"github.com/texttheater/golang-levenshtein/levenshtein"
 )
 
@@ -8,6 +9,7 @@ import (
 func CalculateSimilarity(a, b string) float64 {
 	distance := levenshtein.DistanceForStrings([]rune(a), []rune(b), levenshtein.DefaultOptions)
 	maxLen := maxInt(len(a), len(b))
+	fmt.Printf("a: %s, b: %s, distance: %d, maxLen: %d\n", a, b, distance, maxLen)
 	if maxLen == 0 {
 		return 1.0
 	}
